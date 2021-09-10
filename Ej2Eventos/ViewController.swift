fda6419a0c32fa1e2d142cac80d570db8afb9723
mbeascoa <miguel.beascoabeastek.net>
2021-09-10 12:46:14 +0200

    2 Eventos

--- Ej2Eventos/ViewController.swift
+++ Ej2Eventos/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var miEtiqueta: UILabel!
+    @IBOutlet weak var boton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func mostrarNombre(_ sender: Any) {
+        miEtiqueta.text="Benito Floro"
+        miEtiqueta.textColor=UIColor.blue
+    }
+    
 }
 
